f441a8d0a79060d347b372f4547c0d0874b5d6bc
Your Name <you@example.com>
2024-04-18 21:52:26 +0700

    done

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -4,16 +4,16 @@ import Parallax from "../Parallax/Parallax";
 
 const Hero = () => {
   return (
-    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+    <section className="max-container padding-container flex flex-col gap-20 py-40 pb-32 md:gap-28 lg:py-20 xl:flex-row">
       <div className="hero-map" />
 
-      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+      <div className="relative pt-14 z-20 flex flex-1 flex-col xl:w-1/2">
         <Image
           src="/camp.svg"
           alt="camp"
           width={50}
           height={50}
-          className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
+          className="absolute left-[-5px] top-[20px] w-10 lg:w-[50px]"
         />
         <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
@@ -56,7 +56,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="relative flex flex-1 items-start">
+      <div className="relative flex flex-1  items-center">
         <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
           <div className="flex flex-col">
             <div className="flexBetween">
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,34 +1,40 @@
+"use client";
+import { useState } from "react";
+import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-import { NAV_LINKS } from "@/constants";
-import Button from "../Button/Button";
+import Button from "@/components/Button/Button";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <nav
-      className="max-container padding-container flexBetween
-    relative z-30 py-5    "
-    >
-      {/* LOGO */}
-      <Link href={"/"}>
-        <Image src="/hilink-logo.svg" alt=" logo" width={74} height={29} />
+    <nav className="flexBetween bg-white w-full fixed z-30 px-5 py-3 shadow-2xl">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
 
-      {/* Navigator */}
-      <ul className="hidden h-full gap-12 lg:flex">
-        {NAV_LINKS.map((nav) => (
-          <Link
-            href={nav.href}
-            key={nav.key}
-            className="regular-16 text-grey-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+      <ul
+        className={`lg:h-full lg:relative gap-12 lg:flex ${
+          menuOpen
+            ? "absolute bg-white flex-col justify-center shadow-2xl right-3  top-16 p-5"
+            : "hidden"
+        }`}
+      >
+        {NAV_LINKS.map((link) => (
+          <li
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
           >
-            {nav.label}
-          </Link>
+            <Link href={link.href}>{link.label}</Link>
+          </li>
         ))}
       </ul>
 
-      {/* Button */}
       <div className="lg:flexCenter hidden">
         <Button
           type="button"
@@ -39,12 +45,13 @@ const Navbar = () => {
       </div>
 
       <Image
+        src="menu.svg"
         alt="menu"
-        src="/menu.svg"
-        className="lg:hidden"
         width={32}
         height={32}
-      ></Image>
+        className="inline-block cursor-pointer lg:hidden"
+        onClick={toggleMenu}
+      />
     </nav>
   );
 };
